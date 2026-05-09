d0710071370276c06a094508ead3404d36de44ea
sgaikwad06 <159107905+sgaikwad06@users.noreply.github.com>
2026-05-09 15:28:18 +0530

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StudentMangementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "admin")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                };
+
+                var key = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+                var creds = new SigningCredentials(
+                    key,
+                    SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Issuer"],
+                    audience: null,
+                    claims: claims,
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudentMangementAPI.Data;
+using StudentMangementAPI.Models;
+
+namespace StudentMangementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var students = _context.Students.ToList();
+            return Ok(students);
+        }
+
+        [HttpPost]
+        public IActionResult ActionResult(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+           
+            
+            return Ok("Student Added Successfully");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student updatedStudent)
+        {
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+            {
+                return NotFound("Student Not Found");
+            }
+
+            student.Name = updatedStudent.Name;
+            student.Email = updatedStudent.Email;
+            student.Age = updatedStudent.Age;
+            student.Course = updatedStudent.Course;
+
+            _context.SaveChanges();
+
+            return Ok("Student Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+            {
+                return NotFound("Student Not Found");
+            }
+
+            _context.Students.Remove(student);
+
+            _context.SaveChanges();
+
+            return Ok("Student Deleted Successfully");
+        }
+    }
+}
