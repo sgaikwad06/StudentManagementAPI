e9b4ffa4f1014306f47ab62aef22fe7b3598a799
sgaikwad06 <159107905+sgaikwad06@users.noreply.github.com>
2026-05-09 15:32:49 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentMangementAPI.Models;
+
+
+namespace StudentMangementAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
